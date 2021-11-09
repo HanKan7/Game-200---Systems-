5744985868dd7009b29251c68726252a5397b899
Mia <alicie73@yahoo.com>
2021-11-08 23:07:40 -0800

    EXP bar pt 1

---
+++ Game 200 - Systems Assignment/Assets/ProgressBar.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ProgressBar : MonoBehaviour
+{
+    private Slider slider;
+
+    private void Awake()
+    {
+        slider = gameObject.GetComponent<Slider>();
+    }
+
+    public void incrementProgress(float newprogress)
+    {
+        slider.value += newprogress;
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
