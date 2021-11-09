01de03aef0590e7d5b56d5b40e9682328abe6be7
Raghu Mina <raghumina11@gmail.com>
2021-11-08 22:47:54 -0800

    Audio Files, Meta Files added

---
+++ Game 200 - Systems Assignment/Assets/AudioService.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioService : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public AudioSource bath;
+    public AudioSource play;
+
+    public AudioSource food;
+    public AudioSource gameOver;
+
+    private AudioSource source;
+
+
+    void Awake()
+    {
+        source = GetComponent<AudioSource>();
+    }
+
+    public void PlayBath()
+    {
+        bath.enabled = true;
+        bath.Play();
+    }
+
+    public void PlayFood()
+    {
+        food.enabled = true;
+        food.Play();
+    }
+
+    public void PlayGameOver()
+    {
+        gameOver.enabled = true;
+        gameOver.Play();
+    }
+
+    public void PlayPlay()
+    {
+        play.enabled = true;
+        play.Play();
+    }
+
+}
